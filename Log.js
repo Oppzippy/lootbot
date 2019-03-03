b90c123fb6317ee4d81d1f568e59ccbe3a2fd715
Kyle Scheuing <oppzippy@gmail.com>
2019-03-03 02:21:34 -0500

    Try to not crash when printing errors

--- Log.js
+++ Log.js
@@ -17,9 +17,17 @@ hooks.forEach((funcName) => {
 		args.unshift(dateformat(date, "yyyy-mm-dd hh:mm:ss:")); // add date+time as prefix
 
 		for (let i = 0; i < args.length; i++) {
-			writeStream.write(args[i]);
-			if (i !== args.length) {
-				writeStream.write(" ");
+			try {
+				writeStream.write(args[i]);
+				if (i !== args.length) {
+					writeStream.write(" ");
+				}
+			} catch (err) {
+				// Should never be more than 2x recursion
+				console.err(`Error printing ${typeof args[i]}`);
+				if (typeof args[i] === "object") {
+					console.err(`Failed to print ${args[i].constructor.name}`);
+				}
 			}
 		}
 		writeStream.write("\n");
