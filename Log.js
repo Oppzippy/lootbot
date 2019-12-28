4ef31ba2c6e4f8d1cd1248244d8638ccccf972bf
Kyle Scheuing <oppzippy@gmail.com>
2019-12-28 02:50:21 -0500

    Remove logging to file

--- Log.js
+++ Log.js
@@ -1,11 +1,8 @@
-const fs = require("fs");
+// TODO delete this file
+
 const dateformat = require("dateformat");
 
 const hooks = ["log", "error"];
-
-const writeStream = fs.createWriteStream("app.log", {
-	flags: "a", // append
-});
 
 hooks.forEach(funcName => {
 	const func = console[funcName];
@@ -13,27 +10,6 @@ hooks.forEach(funcName => {
 		const date = new Date();
 		args.unshift(dateformat(date, "yyyy-mm-dd HH:MM:ss:")); // add date+time as prefix
 
-		for (let i = 0; i < args.length; i++) {
-			try {
-				if (args[i]) {
-					const str =
-						typeof args[i] === "object"
-							? JSON.stringify(args[i])
-							: args[i].toString();
-					writeStream.write(str);
-					if (i !== args.length) {
-						writeStream.write(" ");
-					}
-				}
-			} catch (err) {
-				// Should never be more than 2x recursion
-				console.err(`Error printing ${typeof args[i]}`);
-				if (typeof args[i] === "object") {
-					console.err(`Failed to print ${args[i].constructor.name}`);
-				}
-			}
-		}
-		writeStream.write("\n");
 		func.apply(console, args);
 	};
 });
